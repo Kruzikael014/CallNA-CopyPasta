88c5b4fd485033dd553bdcaa8d8cf4cfed254fa1
Marchel Hermanliansyah <MC22-2@slc.net>
2024-10-10 09:08:35 +0700

    Create simple UI for managing messages

---
+++ main.js
@@ -0,0 +1,87 @@
+function copyMessage(message) {
+    navigator.clipboard.writeText(message)
+        .then(() => {
+            alert('Message copied to clipboard!');
+        })
+        .catch(err => {
+            console.error('Failed to copy:', err);
+        });
+}
+
+async function fetchMessage() {
+    try {
+        const response = await fetch('http://localhost:6969/messages');
+        const result = await response.json();
+        if (result.isSuccess) {
+            return result.obj;
+        } else {
+            alert(result.error)
+        }
+    } catch (error) {
+        alert(error)
+    }
+}
+
+async function deleteMessage(messageId) {
+    try {
+        const response = await fetch(`http://localhost:6969/messages/${messageId}`, {
+            method: 'DELETE'
+        });
+        const result = await response.json()
+
+        if (result.isSuccess) {
+            alert('Message has been deleted!')
+        } else {
+            console.log(result);
+            alert('Failed to delete message!');
+        }
+        window.location.reload()
+    } catch (error) {
+        alert('Failed to delete message!');
+    }
+}
+
+async function addMessage() {
+    const messageContent = prompt('Enter the message content:');
+    if (messageContent) {
+        try {
+            const response = await fetch('http://localhost:6969/messages', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ content: messageContent })
+            });
+            const result = await response.json()
+
+            if (result.isSuccess) {
+                alert('Message successfully added!')
+            } else {
+                alert('Failed to add message!');
+            }
+            window.location.reload()
+        } catch (error) {
+            console.log(error);
+            alert('Failed to add message!');
+        }
+    }
+}
+
+window.onload = async function () {
+    const messages = await fetchMessage();
+
+    if (messages) {
+        const messageContainer = document.getElementById('message-container');
+        // messageContainer.innerHTML = '';  // Clear existing messages (if needed)
+
+        messages.forEach(message => {
+            const messageBoxHTML = `
+            <div class="message-box" id="message-${message.message_id}">
+                <button class="copy-btn" onclick="copyMessage('${message.content}')">📋</button>
+                <button class="delete-btn" onclick="deleteMessage('${message.message_id}')">🗑️</button>
+                <p>${message.content}</p>
+            </div>`;
+            messageContainer.innerHTML += messageBoxHTML;
+        });
+    }
+}
